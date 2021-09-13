b81fadca3155db81d850018595798b91e1331243
Jack Lu <mrjacklu@gmail.com>
2021-09-13 22:00:16 +0000

    my first dbt model update

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,10 +1,13 @@
 
+
 /*
-    Welcome to your first dbt model!
+    Welcom to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
-    Try changing "table" to "view" below
+    Try changing "table" to "view" below.
+
+    No, not changing to view, thanks.
 */
 
 {{ config(materialized='table') }}
